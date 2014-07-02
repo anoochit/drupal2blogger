baf53e97c37693e142aef4fb6784bf786509e9c4
Anuchit <anoochit@gmail.com>
2014-07-02 10:51:59 +0700

    fix categoty special char

--- my_data.example.php
+++ my_data.example.php
@@ -1,11 +1,13 @@
 <?php
 // change these variables
-$user="xavier";
-$pass="xxxxxxxxx";
-$db="drupal728";
+$user="username";
+$pass="password";
+$db="drupal";
 $db_prefix="";
 // also change these variables, it should be your blogger info
-$global_author_tag="Anuchit Chalothorn";
+$global_author_tag="<author><name>Anuchit Chalothorn</name><uri>https://plus.google.com/114830002301451896524</uri><email>noreply@blogger.com</email><gd:image rel='http://schemas.google.com/g/2005#thumbnail' width='32' height='32' src='//lh4.googleusercontent.com/-QwLGFVd8EOs/AAAAAAAAAAI/AAAAAAAAOIM/-t8pPp2eacY/s512-c/photo.jpg'/></author>";
 $blogger_id="5985750931254440695";
 $blog_url="http://ossth.blogspot.com";
 ?>
+
+
